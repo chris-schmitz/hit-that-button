8b548ba9314c894f2e5c00e86fc988ad091ee968
chris-schmitz <schmitz.chris@gmail.com>
2017-04-20 13:06:38 -0500

    Adds base project for hit that button

---
+++ app/src/renderer/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div>
+      <h1>Hit!</h1>
+      <h1>That!</h1>
+      <h1>Button!!!</h1>
+    </div>
+    <big-button></big-button>
+  </div>
+</template>
+
+<script>
+  import BigButton from './components/BigButton'
+
+  export default {
+    components: {
+      BigButton
+    }
+  }
+</script>
+
+<style>
+  @import url(https://fonts.googleapis.com/css?family=Lato:300);
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body { height: 100%; }
+
+  body {
+    background:
+      radial-gradient(
+        ellipse at center,
+        rgba(255, 255, 255, 1) 0%,
+        rgba(229, 229, 229, .85) 100%
+      );
+    background-position: center;
+    display: flex;
+    font-family: Lato, Helvetica, sans-serif;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+</style>
+
+<style scoped>
+  #app {
+    display: flex;
+    justify-content: space-around;
+    /*background-color: green;*/
+    flex-direction: column;
+    /*flex: 1;*/
+    /*align-items: stretch;*/
+  }
+
+  #app > * {
+    padding: 20px;
+    /*flex: 1;*/
+    /*border: 1px solid red;*/
+  }
+</style>
---
+++ app/src/renderer/components/BigButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class='big-button-container'>
+    <button
+      @mousedown="buttonUp"
+      @mouseup="hitIt"
+    >Hit me!</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {}
+    },
+    methods: {
+      hitIt () {
+        // Gpio.
+        // component.writeSync(1)
+        // console.log('awwwwyes')
+      },
+      buttonUp () {
+        // component.writeSync(0)
+        // console.log('buttonUp')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .big-button-container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  button {
+    background-color: orangered;
+    color: white;
+    font-size: 50px;
+    font-weight: bold;
+    display:block;
+    height: 300px;
+    width: 300px;
+    border-radius: 50%;
+    border: 1px solid orangered;
+    outline: none;
+    box-shadow: 4px 4px 11px -1px rgba(0,0,0,0.4);
+  }
+
+  button:hover {
+    background-color: orange;
+  }
+
+  button:active {
+    background-color: darkred;
+    box-shadow: 0 0 0 0;
+  }
+</style>
